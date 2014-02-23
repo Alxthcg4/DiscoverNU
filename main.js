5a494b250286a17ec260287f9600b56a4e87d174
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2014-02-23 17:24:30 -0600

    Created prototype of adding and removing filters. Still need to create algorithm for showing filters related to ones currently selected.

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
  */
 
 $(document).ready(function() {
-    alert("Document ready.");
+    availableFilters = ["Cool","Crazy","Hip","Fun","Cultural","Outdoors","Evanston","Family-friendly","Chicago"];
+    addedFilters = [];
     updateFilters();
     $('#new_discovery').click(function(){
         alert("Test!");
@@ -13,15 +14,42 @@ $(document).ready(function() {
 });
 
 function updateFilters() {
-    for (var i = 0;i<12;i++) {
+    currentFilters = ""; $("#filters").html("");
+    for (var i = 0;i<addedFilters.length;i++) {
         currentFilters = $("#filters").html();
         filterString = []
         $("#filters").html(currentFilters+
-            "<div class=\"panel panel-default\">\n\
-                <div class=\"panel-body\">\n\
-                    Filter\n\
-                    <button type=\"button\" class=\"btn btn-primary btn-xs pull-right filter_plus\">+</button>\n\
-                </div>\n\
-            </div>");
+            "<div class=\"panel panel-success\"><div class=\"panel-heading\">"+addedFilters[i]+"<button type=\"button\" class=\"btn btn-primary btn-xs pull-right filter_minus\">-</button></div></div>");
     }
+    for (var j = 0;j<availableFilters.length;j++) {
+        currentFilters = $("#filters").html();
+        filterString = []
+        $("#filters").html(currentFilters+
+            "<div class=\"panel panel-default\"><div class=\"panel-body\">"+availableFilters[j]+"<button type=\"button\" class=\"btn btn-primary btn-xs pull-right filter_plus\">+</button></div></div>");
+    }
+    $(".filter_plus").click(function() {
+        filterText = $(this).parent().text();
+        filterText = filterText.substring(0,filterText.length-1);
+        addedFilters.push(filterText);
+        removeA(availableFilters,filterText);
+        updateFilters();
+    });
+    $(".filter_minus").click(function() {
+        filterText = $(this).parent().text();
+        filterText = filterText.substring(0,filterText.length-1);
+        availableFilters.push(filterText);
+        removeA(addedFilters,filterText);
+        updateFilters();
+    });
+}
+
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax= arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
 }
