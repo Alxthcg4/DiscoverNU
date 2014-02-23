3e29d2ff8b314ac633c28e2018d9c32a6fd1db45
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2014-02-23 15:46:55 -0600

    Made filters through Javascript

--- main.js
+++ main.js
@@ -6,7 +6,22 @@
 
 $(document).ready(function() {
     alert("Document ready.");
+    updateFilters();
     $('#new_discovery').click(function(){
         alert("Test!");
     });
 });
+
+function updateFilters() {
+    for (var i = 0;i<12;i++) {
+        currentFilters = $("#filters").html();
+        filterString = []
+        $("#filters").html(currentFilters+
+            "<div class=\"panel panel-default\">\n\
+                <div class=\"panel-body\">\n\
+                    Filter\n\
+                    <button type=\"button\" class=\"btn btn-primary btn-xs pull-right filter_plus\">+</button>\n\
+                </div>\n\
+            </div>");
+    }
+}
